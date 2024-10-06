6160598fcd80b3d53647fb5771b9abed73b11ec0
thanwanna <thanwanna.dev@gmail.com>
2024-10-06 17:51:30 +0630

    commited in main

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import routes from './routes/index.js';
 import path from 'path';
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -23,14 +26,10 @@ app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 // Routes
 app.use('/api', routes);
 
-// MongoDB connection
-// const mongoURI = "mongodb+srv://thanwanna:449206487!@?mongodbatlas@linkmyanmar.0fvzb.mongodb.net/linkMyanmarDB?retryWrites=true&w=majority&appName=linkMyanmar";
-// const mongoURI = "mongodb+srv://linkMyanmarPassword:linkMyanmarPassword@linkmyanmar.0fvzb.mongodb.net/linkMyanmarDB?retryWrites=true&w=majority&appName=linkMyanmar";
+const mongoURI = process.env.DATABASE_URL as string;
 
-const mongoURI = 'mongodb://localhost:27017/link_myanmar';
 mongoose.connect(mongoURI, {})
     .then(() => console.log('MongoDB connected'))
     .catch((e) => console.log("Error Connecting MongoDB", e));
-
 
 export default app;
