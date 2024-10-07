e333bcfe4e5e8d5b8e34ab88649568bda06f16bc
thanwanna <thanwanna.dev@gmail.com>
2024-10-08 00:18:27 +0630

    mongodb atlas commit

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 // Routes
 app.use('/api', routes);
 
-const mongoURI = process.env.DATABASE_URL as string;
+const mongoURI = process.env.DATABASE_ATLAS_URL as string;
 
 mongoose.connect(mongoURI, {})
     .then(() => console.log('MongoDB connected'))
